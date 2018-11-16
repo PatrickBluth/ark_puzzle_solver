6b14350354454ce4e98cc0f9a279c8ea73d2156c
Patrick <patrick.bluth@gmail.com>
2018-11-16 16:11:39 -0500

    asks for clue updates

--- main.py
+++ main.py
@@ -4,36 +4,47 @@ import itertools
 def key_list_bip39_match(key_word, bip39_word):
     if len(key_word) != len(bip39_word):
         return False
-    for idx,letter in enumerate(key_word):
+    for idx, letter in enumerate(key_word):
         if letter != '_':
             if letter != bip39_word[idx]:
                 return False
     return True
 
 
-f = open('bip39_words.txt')
-bip39 = f.readlines()
-f.close()
-bip39 = [w.replace('\n', '') for w in bip39]
+def generate_passphrases():
+    f = open('bip39_words.txt')
+    bip39 = f.readlines()
+    f.close()
+    bip39 = [w.replace('\n', '') for w in bip39]
 
-f = open('clues.txt')
-key_list = f.readlines()
-f.close()
-key_list = [w.replace('\n', '') for w in key_list]
+    f = open('clues.txt')
+    key_list = f.readlines()
+    f.close()
+    key_list = [w.replace('\n', '') for w in key_list]
 
-possible_words = [[] for i in range(12)]
-for i in range(12):
-    for word in bip39:
-        if key_list_bip39_match(key_list[i], word):
-            possible_words[i].append(word)
+    possible_words = [[] for i in range(12)]
+    for i in range(12):
+        for word in bip39:
+            if key_list_bip39_match(key_list[i], word):
+                possible_words[i].append(word)
 
-res = [' '.join(str(y) for y in x) for x in itertools.product(*possible_words)]
-print(len(res))
+    res = [' '.join(str(y) for y in x) for x in itertools.product(*possible_words)]
 
-f = open('passphrases.txt', 'w')
-for line in res:
-    f.write(str(line))
-    f.write('\n')
-f.close()
+    f = open('passphrases.txt', 'w')
+    for line in res:
+        f.write(str(line))
+        f.write('\n')
+    f.close()
+
+    print('Update clue list based on provided hints to narrow results.')
+    print('Possible words: ')
+    for word_list in possible_words:
+        print(word_list)
+    input('\nPress any key to exit')
 
 
+def main():
+    generate_passphrases()
+
+
+main()
