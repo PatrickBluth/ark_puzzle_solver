eb4e9f2d5afc61a2051c42f8b1d5e113694a797c
Patrick <patrick.bluth@gmail.com>
2018-11-16 16:20:45 -0500

    simple error handling

--- main.py
+++ main.py
@@ -22,11 +22,16 @@ def generate_passphrases():
     f.close()
     key_list = [w.replace('\n', '') for w in key_list]
 
+    if len(key_list) != 12 or '' in key_list:
+        raise ValueError('Please make sure "clues.txt" only has 12 words, one on each line.')
+
     possible_words = [[] for i in range(12)]
     for i in range(12):
         for word in bip39:
             if key_list_bip39_match(key_list[i], word):
                 possible_words[i].append(word)
+        if len(possible_words[i]) == 0:
+            raise ValueError('Could not match any words to clue "{}". Please make sure the clue was entered correctly.'.format(key_list[i]))
 
     res = [' '.join(str(y) for y in x) for x in itertools.product(*possible_words)]
 
@@ -40,7 +45,8 @@ def generate_passphrases():
     print('Possible words: ')
     for word_list in possible_words:
         print(word_list)
-    input('\nPress any key to exit')
+    print('\nTotal number of possible passphrases: {}'.format(len(res)))
+    input('\nPress enter to exit')
 
 
 def main():
