1ccaa63310a41a4ac3e2c57f95db866a73043e25
Patrick <patrick.bluth@gmail.com>
2018-11-18 11:48:41 -0500

    switched itertools.prod to for loops

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def key_list_bip39_match(key_word, bip39_word):
     return True
 
 
-def generate_passphrases():
+def generate_passphrases(address):
     f = open('bip39_words.txt')
     bip39 = f.readlines()
     f.close()
@@ -28,23 +28,44 @@ def generate_passphrases():
     f.close()
     key_list = [w.replace('\n', '') for w in key_list]
 
-    if len(key_list) != 12 or '' in key_list:
+    if len(key_list) != 11 or '' in key_list:
         raise ValueError('Please make sure "clues.txt" only has 12 words, one on each line.')
 
     possible_words = [[] for i in range(12)]
-    for i in range(12):
+    for i in range(11):
         for word in bip39:
             if key_list_bip39_match(key_list[i], word):
                 possible_words[i].append(word)
         if len(possible_words[i]) == 0:
             raise ValueError('Could not match any words to clue "{}". Please make sure the clue was entered correctly.'.format(key_list[i]))
 
-    res = [' '.join(str(y) for y in x) for x in itertools.product(*possible_words)]
+    for word in bip39:
+        possible_words[11].append(word)
 
-    print('Possibilities for each clue:')
-    for line in possible_words:
-        print(line)
-    return res
+    # res = [' '.join(str(y) for y in x) for x in itertools.product(*possible_words)]
+
+    for word1 in possible_words[0]:
+        for word2 in possible_words[1]:
+            for word3 in possible_words[2]:
+                for word4 in possible_words[3]:
+                    for word5 in possible_words[4]:
+                        for word6 in possible_words[5]:
+                            for word7 in possible_words[6]:
+                                for word8 in possible_words[7]:
+                                    for word9 in possible_words[8]:
+                                        for word10 in possible_words[9]:
+                                            for word11 in possible_words[10]:
+                                                for word12 in possible_words[11]:
+                                                    passphrase = word1 + ' ' + word2 + ' ' + word3 + ' ' + word4 + ' ' + word5 + ' ' + word6 + ' ' + word7 + ' ' + word8 + ' ' + word9 + ' ' + word10 + ' ' + word11 + ' ' + word12
+                                                    # passphrase = ' '.join(str(y) for y in x)
+                                                    corresponding_address = address_from_passphrase(passphrase)
+                                                    if corresponding_address == address:
+                                                        return passphrase
+
+    # print('Possibilities for each clue:')
+    # for line in possible_words:
+    #     print(line)
+
 
 
 def find_correct_passphrase(desired_address, phassphrases):
@@ -60,10 +81,6 @@ def find_correct_passphrase(desired_address, phassphrases):
 
 
 def main():
-    passphrases = generate_passphrases()
-    print('\nNumber of passphrases to test: {}'.format(len(passphrases)))
-    print('Estimated completion time: {} seconds'.format(round(len(passphrases)/30000), 2))
-    print('Exit and update clues.txt to narrow search, or continue and input target address\n')
     while True:
         try:
             address = input('Enter the desired address.')
@@ -72,9 +89,16 @@ def main():
             print('Invalid address. Please double check the target address and try again.')
         else:
             break
-    passphrase = find_correct_passphrase(address, passphrases)
-    print('Passphrase is: {}'.format(passphrase))
+    passphrase = generate_passphrases(address)
+    print('The passphrase is: {}'.format(passphrase))
     input('Press enter to quit')
+    # print('\nNumber of passphrases to test: {}'.format(len(passphrases)))
+    # print('Estimated completion time: {} seconds'.format(round(len(passphrases)/30000), 2))
+    # print('Exit and update clues.txt to narrow search, or continue and input target address\n')
+    #
+    # passphrase = find_correct_passphrase(address, passphrases)
+    # print('Passphrase is: {}'.format(passphrase))
+    # input('Press enter to quit')
 
 
 main()
